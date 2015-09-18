5f0ece2af5b52661a7a10c37ff756702a5cd2150
erikvw <ew2789@gmail.com>
2015-09-18 08:39:20 +0200

    add mask inprt

--- edc_base/encrypted_fields.py
+++ edc_base/encrypted_fields.py
@@ -8,16 +8,24 @@ if get_version().startswith('1.6') and six.PY2:
             EncryptedFirstnameField as FirstnameField,
             EncryptedLastnameField as LastnameField,
             EncryptedCharField, EncryptedTextField,
-            EncryptedIdentityField as IdentityField)
+            EncryptedIdentityField as IdentityField,
+            EncryptedDecimalField)
         from edc.base.model.fields import IdentityTypeField
         from edc.base.model.fields import IsDateEstimatedField
-    except ImportError:
+        from edc.core.crypto_fields.classes import FieldCryptor
+        from edc.core.crypto_fields.utils import mask_encrypted
+    except ImportError as e:
+        print(e)
         from edc_base.model.fields import IdentityTypeField
         from edc_base.model.fields import IsDateEstimatedField
+        from django_crypto_fields.classes import FieldCryptor
+        from django_crypto_fields.utils import mask_encrypted
         from django_crypto_fields.fields import (
             FirstnameField, LastnameField, EncryptedCharField, EncryptedTextField, IdentityField)
 else:
     from edc_base.model.fields import IdentityTypeField
     from edc_base.model.fields import IsDateEstimatedField
+    from django_crypto_fields.classes import FieldCryptor
+    from django_crypto_fields.utils import mask_encrypted
     from django_crypto_fields.fields import (
         FirstnameField, LastnameField, EncryptedCharField, EncryptedTextField, IdentityField)
