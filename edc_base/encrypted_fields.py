95ffcecbefff87fe73985d26ba496d1b7723fc2e
erikvw <ew2789@gmail.com>
2015-12-26 10:15:20 +0200

    add warning on version imports

--- edc_base/encrypted_fields.py
+++ edc_base/encrypted_fields.py
@@ -1,9 +1,24 @@
 import six
 from distutils.version import StrictVersion
 from django import get_version
+from django.conf import settings
 
 from edc_base.model.fields import IdentityTypeField
 from edc_base.model.fields import IsDateEstimatedField
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    import django_crypto_fields
+    if 'django_crypto_fields' not in settings.INSTALLED_APPS:
+        raise ImproperlyConfigured(
+            'Why is the package \'django_crypto_fields\' installed? '
+            'Choosing the wrong encryption package can lead to data loss. '
+            'If you mean to use it, add it to INSTALLED_APPS, otherwise uninstall it '
+            'with \'pip uninstall django_crypto_fields\'.')
+except ImportError:
+    pass
+
+
 if StrictVersion(get_version()) < StrictVersion('1.8.0') and six.PY2:
     try:
         from edc.core.crypto_fields.classes import FieldCryptor
