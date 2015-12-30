a464bd8a6f4c802fe8fdf524ac0aa8f0baaa676a
erikvw <ew2789@gmail.com>
2015-12-30 11:41:26 +0200

    fix import handling in encrypted fields

--- edc_base/encrypted_fields.py
+++ edc_base/encrypted_fields.py
@@ -1,6 +1,3 @@
-import six
-from distutils.version import StrictVersion
-from django import get_version
 from django.conf import settings
 
 from edc_base.model.fields import IdentityTypeField
@@ -19,23 +16,18 @@ except ImportError:
     pass
 
 
-if StrictVersion(get_version()) < StrictVersion('1.8.0') and six.PY2:
-    try:
-        from edc_crypto_fields.classes import FieldCryptor
-        from edc_crypto_fields.fields import (
-            EncryptedFirstnameField as FirstnameField,
-            EncryptedLastnameField as LastnameField,
-            EncryptedCharField, EncryptedTextField,
-            EncryptedIdentityField as IdentityField,
-            EncryptedDecimalField, BaseEncryptedField)
-        # from edc_crypto_fields.models import Crypt  # causes import error
-    except ImportError as e:
-        from django_crypto_fields.classes import FieldCryptor
-        from django_crypto_fields.fields import (
-            FirstnameField, LastnameField, EncryptedCharField, EncryptedTextField, IdentityField)
-else:
+try:
+    from edc_crypto_fields.classes import FieldCryptor
+    from edc_crypto_fields.fields import (
+        EncryptedFirstnameField as FirstnameField,
+        EncryptedLastnameField as LastnameField,
+        EncryptedCharField, EncryptedTextField,
+        EncryptedIdentityField as IdentityField,
+        EncryptedDecimalField, BaseEncryptedField)
+    # from edc_crypto_fields.models import Crypt  # causes import error
+except ImportError as e:
+    print(str(e))
     from django_crypto_fields.classes import FieldCryptor
-    # from django_crypto_fields.utils import mask_encrypted
     from django_crypto_fields.fields import (
         FirstnameField, LastnameField, EncryptedCharField, EncryptedTextField, IdentityField,
         BaseField as BaseEncryptedField)
