58949d250d5009b128d18486865a1bbe87bf811d
erikvw <ew2789@gmail.com>
2016-01-13 14:43:31 +0200

    put age related funcs in one file

--- edc_base/utils/__init__.py
+++ edc_base/utils/__init__.py
@@ -1,5 +1,4 @@
-from .age import age, formatted_age
+from .age import age, formatted_age, get_age_in_days
 from .convert_from_camel import convert_from_camel
-from .get_age_in_days import get_age_in_days
 from .get_safe_random_string import get_safe_random_string, safe_allowed_chars
 from .round_up import round_up
