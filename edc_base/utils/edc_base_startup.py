28a2d457f2bdddfb6912b0c565164940a58f9617
erikvw <ew2789@gmail.com>
2016-01-22 13:49:55 +0200

    remove setting get_visit, leave to class

--- edc_base/utils/edc_base_startup.py
+++ edc_base/utils/edc_base_startup.py
@@ -10,6 +10,14 @@ def edc_base_startup(verbose=None):
                 _discover_visit_model_if_exists(model)
             except AttributeError:
                 pass
+#         for model in get_models(app):
+#             try:
+#                 parent_model = _get_inline_parent_model(model)
+#                 model.get_visit = lambda self: getattr(getattr(self, model.fk_model_attr),
+#                                                        parent_model.visit_model_attr)
+#             except AttributeError as e:
+#                 if 'fk_model_attr' not in str(e):
+#                     raise AttributeError(e)
 
 
 def _get_model_if_tuple(model):
@@ -26,10 +34,6 @@ def _discover_visit_model_if_exists(model):
     """Set model class attributes `visit_model`, `visit_model_attr` and `natural_key.dependencies`."""
     try:
         model.visit_model, model.visit_model_attr = _configure_visit_model_attrs(model)
-        try:
-            model.get_visit = lambda self: getattr(self, model.visit_model_attr)
-        except TypeError as e:
-            raise TypeError(e, 'Specify the visit_model_attr on {}'.format(model))
         model.natural_key.dependencies = ['{}.{}'.format(
             model.visit_model._meta.app_label,
             model.visit_model._meta.model_name)]
@@ -54,3 +58,8 @@ def _configure_visit_model_attrs(model):
     except AttributeError:
         pass
     return visit_model, visit_model_attr
+
+
+def _get_inline_parent_model(model):
+    """Discover and return the visit_model and visit_model_attr from the model class."""
+    return [fld for fld in model._meta.fields if fld.name == model.fk_model_attr][0].rel.to
