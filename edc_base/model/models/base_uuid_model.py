031a1107c398eaec2311247ae0d6aca0c89a67fb
erikvw <ew2789@gmail.com>
2016-06-25 13:45:07 +0200

    just use django's uuid field for default id

--- edc_base/model/models/base_uuid_model.py
+++ edc_base/model/models/base_uuid_model.py
@@ -1,16 +1,13 @@
 import uuid
 
-from .base_model import BaseModel
 from django.db.models.fields import UUIDField
+
+from .base_model import BaseModel
 
 
 class BaseUuidModel(BaseModel):
 
     """Base model class for all models using an UUID and not an INT for the primary key. """
-
-#     id = UUIDAutoField(
-#         primary_key=True,
-#         editable=False)
 
     id = UUIDField(
         blank=True,
