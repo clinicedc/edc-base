6534c71d0c7e13ced78d0d7bbafdb9063d0635b4
erikvw <ew2789@gmail.com>
2016-07-31 22:14:28 +0200

    change APP_LABEL to APP_CONFIG_NAME

--- edc_base/views/edc_base_view_mixin.py
+++ edc_base/views/edc_base_view_mixin.py
@@ -4,24 +4,26 @@ from django.apps import apps as django_apps
 from django.conf import settings
 from django_revision.views import RevisionMixin
 from django.core.exceptions import ImproperlyConfigured
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
+# from django.utils.decorators import method_decorator
+# from django.contrib.auth.decorators import login_required
 
 
 class EdcBaseViewMixin(RevisionMixin):
     """Mixes in common template variables for the footer, etc."""
 
-    app_label = None  # this is the main app (e.g. where settings resides)
+    app_config_name = None  # this is the main app that defines institution, etc
 
     @property
-    def app(self):
+    def main_app_config(self):
         try:
-            app_label = settings.APP_LABEL
+            name = settings.APP_CONFIG_NAME
         except AttributeError:
-            app_label = self.app_label
-        if not app_label:
-            raise ImproperlyConfigured('Main APP_LABEL not set. Either set on view or in settings.APP_LABEL.')
-        return django_apps.get_app_config(app_label)
+            name = self.app_config_name
+        if not name:
+            raise ImproperlyConfigured(
+                'Main AppConfig "name" is needed for AppConfig attributes \'verbose_name\', \'institution\', etc. '
+                'Add to settings APP_CONFIG_NAME = your_app_name.')
+        return django_apps.get_app_config(name)
 
 #     @method_decorator(login_required)
 #     def dispatch(self, *args, **kwargs):
@@ -30,8 +32,8 @@ class EdcBaseViewMixin(RevisionMixin):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update({
-            'project_name': self.app.verbose_name,
-            'institution': self.app.institution,
+            'project_name': self.main_app_config.verbose_name,
+            'institution': self.main_app_config.institution,
             'year': date.today().year,
         })
         return context
