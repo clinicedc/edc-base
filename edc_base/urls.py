132324cbf1ae343d56a572c643e5497a387e7f4c
erikvw <ew2789@gmail.com>
2016-07-31 22:15:10 +0200

    comment out js_reverse url

--- edc_base/urls.py
+++ edc_base/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+# from django_js_reverse.views import urls_js
 from edc_base.views import LoginView, LogoutView
 
 """
@@ -14,6 +15,7 @@ Add to your app as
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    # url(r'^jsreverse/$', urls_js, name='js_reverse'),
     url(r'accounts/login', LoginView.as_view(), name='login_url'),
     url(r'login', LoginView.as_view(), name='login_url'),
     url(r'logout', LogoutView.as_view(), name='logout_url'),
