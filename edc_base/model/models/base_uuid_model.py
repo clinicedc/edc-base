5bd9b3d79245c3252e4eedfbc2ecf437394d3bf7
erikvw <ew2789@gmail.com>
2016-08-05 19:47:34 +0200

    remove unused code

--- edc_base/model/models/base_uuid_model.py
+++ edc_base/model/models/base_uuid_model.py
@@ -1,6 +1,3 @@
-# import uuid
-
-# from django.db.models.fields import UUIDField
 from edc_base.model.fields.uuid_auto_field import UUIDAutoField
 
 from .base_model import BaseModel
@@ -15,14 +12,6 @@ class BaseUuidModel(BaseModel):
         editable=False,
         help_text="System auto field. UUID primary key.",
         primary_key=True)
-
-#     id = UUIDAutoField(
-#         blank=True,
-#         default=uuid.uuid4,
-#         editable=False,
-#         help_text="System auto field. UUID primary key.",
-#         primary_key=True,
-#     )
 
     class Meta:
         abstract = True
