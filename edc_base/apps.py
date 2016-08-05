fc549f620f9a1b5b282717cb3041ac94a9053d02
erikvw <ew2789@gmail.com>
2016-08-05 19:47:14 +0200

    add project name to app config

--- edc_base/apps.py
+++ edc_base/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig as DjangoAppConfig
 class AppConfig(DjangoAppConfig):
     name = 'edc_base'
     institution = 'My Institution'
-    verbose_name = 'My Project Title'
+    project_name = 'My Project Title'
