d48ad7f97088567db1208e414043d8b0a6476a74
erikvw <ew2789@gmail.com>
2016-08-09 13:42:26 +0200

    add warning for debug=True

--- edc_base/views/edc_base_view_mixin.py
+++ edc_base/views/edc_base_view_mixin.py
@@ -1,17 +1,12 @@
 from datetime import date
 
 from django.apps import apps as django_apps
+from django.conf import settings
 from django_revision.views import RevisionMixin
-# from django.utils.decorators import method_decorator
-# from django.contrib.auth.decorators import login_required
 
 
 class EdcBaseViewMixin(RevisionMixin):
     """Mixes in common template variables for the footer, etc."""
-
-#     @method_decorator(login_required)
-#     def dispatch(self, *args, **kwargs):
-#         return super(EdcBaseViewMixin, self).dispatch(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -20,5 +15,6 @@ class EdcBaseViewMixin(RevisionMixin):
             'project_name': app_config.project_name,
             'institution': app_config.institution,
             'year': date.today().year,
+            'DEBUG': settings.DEBUG,
         })
         return context
