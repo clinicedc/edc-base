78631c2814c3f082c684b2dc9fbc7b24c2ed4dff
erikvw <ew2789@gmail.com>
2016-08-23 12:41:05 +0200

    remove base admin classes, add to mixins

--- edc_base/modeladmin/admin/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .base_model_admin import BaseModelAdmin
-from .base_stacked_inline import BaseStackedInline
-from .base_tabular_inline import BaseTabularInline
-from .limited_admin_inline_mixin import LimitedAdminInlineMixin
