3570f2f445fe4dc1bf7c78d93a145c8bbd0d0d3b
erikvw <ew2789@gmail.com>
2016-11-02 13:36:08 -0500

    read only mixin modify getform to change widgets on related and dates

--- edc_base/modeladmin/mixins.py
+++ edc_base/modeladmin/mixins.py
@@ -4,6 +4,8 @@ from django.core.urlresolvers import reverse
 from django.http.response import HttpResponseRedirect
 from django.utils import timezone
 from django.utils.html import format_html
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms.widgets import DateInput
 
 
 class ModelAdminBasicMixin:
@@ -458,6 +460,21 @@ class ModelAdminReadOnlyMixin:
         to the admin url querystring add "next" and "edc_readonly=1"
 
     """
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(ModelAdminReadOnlyMixin, self).get_form(request, obj, **kwargs)
+        if request.GET.get('edc_readonly'):
+            for form_field in form.base_fields.values():
+                form_field.disabled = True
+                try:
+                    form_field.widget.can_add_related = False
+                    form_field.widget.can_change_related = False
+                    form_field.widget.can_delete_related = False
+                except AttributeError:
+                    pass
+                if isinstance(form_field.widget, AdminDateWidget):
+                    form_field.widget = DateInput()
+        return form
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         extra_context = extra_context or {}
