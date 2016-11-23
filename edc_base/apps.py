d0d4a51f22415c8efd0eb3cba086ad69c7721209
erikvw <ew2789@gmail.com>
2016-11-23 07:31:51 +0200

    add foreignkey contraints by default if using sqlite3

--- edc_base/apps.py
+++ edc_base/apps.py
@@ -1,7 +1,18 @@
 from django.apps import AppConfig as DjangoAppConfig
+from django.db.backends.signals import connection_created
+
+
+def activate_foreign_keys(sender, connection, **kwargs):
+    """Enable integrity constraint with sqlite."""
+    if connection.vendor == 'sqlite':
+        cursor = connection.cursor()
+        cursor.execute('PRAGMA foreign_keys = ON;')
 
 
 class AppConfig(DjangoAppConfig):
     name = 'edc_base'
     institution = 'My Institution'
     project_name = 'My Project Title'
+
+    def ready(self):
+        connection_created.connect(activate_foreign_keys)
