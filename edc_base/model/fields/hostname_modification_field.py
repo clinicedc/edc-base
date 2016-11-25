cec37e8dea24e342a7548b0e1be6b5f935f886c6
erikvw <ew2789@gmail.com>
2016-11-25 13:14:59 +0200

    add default for blank

--- edc_base/model/fields/hostname_modification_field.py
+++ edc_base/model/fields/hostname_modification_field.py
@@ -8,20 +8,12 @@ class HostnameModificationField (CharField):
 
     description = _("Custom field for hostname modified")
 
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('blank', True)
+        CharField.__init__(self, *args, **kwargs)
+
     def pre_save(self, model_instance, add):
         """Updates socket.gethostname() on each save."""
         value = socket.gethostname()
         setattr(model_instance, self.attname, value)
         return value
-
-    def get_internal_type(self):
-        return "CharField"
-
-    def south_field_triple(self):
-        "Returns a suitable description of this field for South."
-        # We'll just introspect the _actual_ field.
-        from south.modelsinspector import introspector
-        field_class = "django.db.models.fields.CharField"
-        args, kwargs = introspector(self)
-        # That's our definition!
-        return (field_class, args, kwargs)
