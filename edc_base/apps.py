30696db3dd4ca1255da18c26d9e318256cd64461
erikvw <ew2789@gmail.com>
2016-12-02 00:03:25 +0200

    warn about time zone if not UTC

--- edc_base/apps.py
+++ edc_base/apps.py
@@ -1,5 +1,11 @@
+import sys
+
 from django.apps import AppConfig as DjangoAppConfig
+from django.conf import settings
 from django.db.backends.signals import connection_created
+from django.core.management.color import color_style
+
+style = color_style()
 
 
 def activate_foreign_keys(sender, connection, **kwargs):
@@ -15,4 +21,10 @@ class AppConfig(DjangoAppConfig):
     project_name = 'My Project Title'
 
     def ready(self):
+        sys.stdout.write('Loading {} ...\n'.format(self.verbose_name))
         connection_created.connect(activate_foreign_keys)
+        tzname = settings.TIME_ZONE
+        sys.stdout.write(' * TIME ZONE {}.\n'.format(settings.TIME_ZONE))
+        if tzname != 'UTC':
+            sys.stdout.write(style.WARNING(' * Warning: timezone is not UTC!\n'))
+        sys.stdout.write(' Done loading {}.\n'.format(self.verbose_name))
