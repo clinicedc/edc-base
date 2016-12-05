529e78c0e1db2964bb1b0c27d524a5cc6bf162cf
erikvw <ew2789@gmail.com>
2016-12-05 11:20:53 +0200

    move tz_detect to bottom of body tag

--- edc_base/settings.py
+++ edc_base/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'tz_detect.middleware.TimezoneMiddleware',
 ]
 
 ROOT_URLCONF = 'edc_base.urls'
