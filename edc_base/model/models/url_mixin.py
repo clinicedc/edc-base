be9635fb56d0235a69b613105965eca5b260b102
erikvw <ew2789@gmail.com>
2016-12-08 14:40:16 +0200

    add protperty to admin_url_name

--- edc_base/model/models/url_mixin.py
+++ edc_base/model/models/url_mixin.py
@@ -4,22 +4,30 @@ from django.db import models
 
 class UrlMixin(models.Model):
 
-    ADMIN_SITE_NAME = 'admin'
+    ADMIN_SITE_NAME = None
 
     def get_absolute_url(self):
         if self.id:
-            url = reverse('{admin_site}:{app_label}_{object_name}_change'.format(
-                admin_site=self.ADMIN_SITE_NAME,
+            url = reverse(self.admin_url_name, args=(str(self.id),))
+        else:
+            url = reverse(self.admin_url_name)
+        return url
+
+    @property
+    def admin_url_name(self):
+        """Returns the django admin add or change url name."""
+        if self.id:
+            admin_url_name = '{admin_site}:{app_label}_{object_name}_change'.format(
+                admin_site=self.ADMIN_SITE_NAME or 'admin',
                 app_label=self._meta.app_label,
                 object_name=self._meta.object_name.lower()
-            ), args=(str(self.id),))
+            )
         else:
-            url = reverse('{admin_site}:{app_label}_{object_name}_add'.format(
-                admin_site=self.ADMIN_SITE_NAME,
+            admin_url_name = '{admin_site}:{app_label}_{object_name}_add'.format(
+                admin_site=self.ADMIN_SITE_NAME or 'admin',
                 app_label=self._meta.app_label,
                 object_name=self._meta.object_name.lower())
-            )
-        return url
+        return admin_url_name
 
     class Meta:
         abstract = True
