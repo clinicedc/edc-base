eab3446fb95b02012864444c4f39958d37b4ba38
erikvw <ew2789@gmail.com>
2016-12-20 18:01:47 +0200

    add additional eceptions for clarity

--- edc_base/test_mixins/reference_date_mixin.py
+++ edc_base/test_mixins/reference_date_mixin.py
@@ -10,18 +10,22 @@ class ReferenceDateMixin:
 
     consent_model = None
 
-    @property
-    def test_mixin_reference_datetime(self):
+    def get_utcnow(self):
+        """Returns a datetime that is the earliest date of consent allowed for the consent model.
+
+        Note: this date is defined in edc_consent.apps ConsentConfig."""
         app_config = django_apps.get_app_config('edc_consent')
         test_mixin_reference_datetime = {}
         for consent_config in app_config.consent_configs:
             test_mixin_reference_datetime.update({consent_config.label_lower: consent_config.start})
         if not test_mixin_reference_datetime:
             raise TestMixinError('Cannot determine a reference date from get_utcnow')
-        return test_mixin_reference_datetime
-
-    def get_utcnow(self):
-        """Returns a datetime that is the earliest date of consent allowed for the consent model.
-
-        Note: this date is defined in edc_consent.apps ConsentConfig."""
-        return self.test_mixin_reference_datetime.get(self.consent_model)
+        if self.consent_model not in test_mixin_reference_datetime:
+            raise TestMixinError(
+                'Cannot determine a reference date from get_utcnow. No config for \'{}\'. '
+                'Expected one of {}.See AppConfig and your TestMixin class'.format(
+                    self.consent_model, [c.label_lower for c in app_config.consent_configs]))
+        utcnow = test_mixin_reference_datetime.get(self.consent_model)
+        if not utcnow:
+            raise TestMixinError('Cannot determine a reference date from get_utcnow. Got {}.'.format(utcnow))
+        return utcnow
