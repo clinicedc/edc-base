089524d96f988dd4cf39e24d0dfb9b1ee6ae21f7
erikvw <ew2789@gmail.com>
2016-12-30 14:39:51 +0200

    convert to arrow

--- edc_base/model/validators/date.py
+++ edc_base/model/validators/date.py
@@ -1,29 +1,31 @@
+import arrow
+
 from datetime import timedelta
 
 from edc_base.exceptions import FutureDateError, NotFutureDateError
 
-from ...utils import get_utcnow
-
 
 def datetime_not_future(value):
+    value_utc = arrow.Arrow.fromdatetime(value, value.tzinfo).to('utc').datetime
     time_error = timedelta(minutes=10)
-    if value > get_utcnow() + time_error:
+    if value_utc > arrow.utcnow() + time_error:
         raise FutureDateError(u'Datetime cannot be a future date and time. You entered {}'.format(value))
 
 
 def date_not_future(value):
-    now = get_utcnow().date()
-    if value > now:
+    value_utc = arrow.Arrow.fromdatetime(value, value.tzinfo).to('utc').date
+    if value_utc > arrow.utcnow().date():
         raise FutureDateError(u'Date cannot be a future date. You entered {}'.format(value))
 
 
 def datetime_is_future(value):
+    value_utc = arrow.Arrow.fromdatetime(value, value.tzinfo).to('utc').datetime
     time_error = timedelta(minutes=10)
-    if value < get_utcnow() + time_error:
+    if value_utc < arrow.utcnow() + time_error:
         raise NotFutureDateError(u'Datetime must be a future date and time. You entered {}'.format(value))
 
 
 def date_is_future(value):
-    now = get_utcnow().date()
-    if value < now:
+    value_utc = arrow.Arrow.fromdatetime(value, value.tzinfo).to('utc').date
+    if value_utc < arrow.utcnow().date():
         raise NotFutureDateError(u'Date must be a future date. You entered {}'.format(value))
