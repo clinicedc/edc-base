b0a03ed6730d1c8c4518d5bdc41ef182b8b316d6
erikvw <ew2789@gmail.com>
2017-01-04 21:47:50 +0200

    allow filter to fail silently, fix setup of consents

--- edc_base/templatetags/edc_base_extras.py
+++ edc_base/templatetags/edc_base_extras.py
@@ -1,5 +1,6 @@
 from django import template
 
+from ..exceptions import AgeValueError
 from ..utils import age
 
 register = template.Library()
@@ -7,4 +8,9 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def age_in_years(context, born):
-    return age(born, context['reference_datetime']).years
+    age_in_years = None
+    try:
+        age_in_years = age(born, context.get('reference_datetime')).years
+    except AgeValueError:
+        age_in_years = None
+    return age_in_years or born
--- edc_base_test/mixins/dates_test_mixin.py
+++ edc_base_test/mixins/dates_test_mixin.py
@@ -1,6 +1,6 @@
 import arrow
 import sys
-
+import copy
 from dateutil.relativedelta import relativedelta
 
 from django.apps import apps as django_apps
@@ -44,16 +44,17 @@ class DatesTestMixin:
         sys.stdout.write(style.NOTICE(' * test study open datetime: {}\n'.format(study_open_datetime)))
         sys.stdout.write(style.NOTICE(' * test study close datetime: {}\n'.format(study_close_datetime)))
         testconsents = []
-        for consent in site_consents.registry:
-            tdelta = consent.start - study_open_datetime
-            consent_period_tdelta = consent.end - consent.start
-            consent.start = consent.start - tdelta
-            consent.end = consent.start + consent_period_tdelta - timedelta(minutes=24 * 60)
-            sys.stdout.write(style.NOTICE(' * {}: {} - {}\n'.format(consent.name, consent.start, consent.end)))
-            testconsents.append(consent)
+        tdelta = site_consents.consents[0].start - study_open_datetime
+        for consent in site_consents.consents:
+            test_consent = copy.copy(consent)
+            test_consent.start = consent.start - tdelta
+            test_consent.end = consent.end - tdelta
+            sys.stdout.write(style.NOTICE(
+                ' * {}: {} - {}\n'.format(test_consent.name, test_consent.start, test_consent.end)))
+            testconsents.append(test_consent)
         site_consents.backup_registry()
-        for consent in testconsents:
-            site_consents.register(consent)
+        for test_consent in testconsents:
+            site_consents.register(test_consent)
 
     @classmethod
     def tearDownClass(cls):
