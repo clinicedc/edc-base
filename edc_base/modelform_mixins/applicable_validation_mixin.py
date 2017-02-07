d103ca033d632f247e1cc58684012be8291e8eef
erikvw <ew2789@gmail.com>
2017-02-07 14:01:32 +0200

    update validator class

--- edc_base/modelform_mixins/applicable_validation_mixin.py
+++ edc_base/modelform_mixins/applicable_validation_mixin.py
@@ -5,42 +5,47 @@ from edc_constants.constants import NOT_APPLICABLE
 
 class ApplicableValidationMixin:
 
-    def applicable_if(self, response=None, field=None, field_applicable=None):
-        return self.applicable(response, field, field_applicable)
+    def applicable_if(self, *responses, field=None, field_applicable=None):
+        return self.applicable(
+            *responses, field=field, field_applicable=field_applicable)
 
-    def not_applicable_if(self, response=None, field=None, field_applicable=None):
-        return self.not_applicable(response, field, field_applicable)
+    def not_applicable_if(self, *responses, field=None, field_applicable=None):
+        return self.not_applicable(
+            *responses, field=field, field_applicable=field_applicable)
 
-    def applicable(self, response=None, field=None, field_applicable=None):
+    def applicable(self, *responses, field=None, field_applicable=None):
         """Returns False or raises a validation error for field
         pattern where response to question 1 makes
         question 2 applicable.
         """
         cleaned_data = self.cleaned_data
-        if (cleaned_data.get(field) == response
+        print(responses)
+        print('field={}'.format(field))
+        print('field_applicable={}'.format(field_applicable))
+        if (cleaned_data.get(field) in responses
                 and cleaned_data.get(field_applicable) == NOT_APPLICABLE):
             raise forms.ValidationError({
                 field_applicable:
                 'This field is applicable'})
-        elif (cleaned_data.get(field) != response
+        elif (cleaned_data.get(field) not in responses
                 and cleaned_data.get(field_applicable) != NOT_APPLICABLE):
             raise forms.ValidationError({
                 field_applicable:
                 'This field is not applicable'})
         return False
 
-    def not_applicable(self, response=None, field=None, field_applicable=None):
+    def not_applicable(self, *responses, field=None, field_applicable=None):
         """Returns False or raises a validation error for field
         pattern where response to question 1 makes
         question 2 NOT applicable.
         """
         cleaned_data = self.cleaned_data
-        if (cleaned_data.get(field) == response
+        if (cleaned_data.get(field) in responses
                 and cleaned_data.get(field_applicable) != NOT_APPLICABLE):
             raise forms.ValidationError({
                 field_applicable:
                 'This field is not applicable'})
-        elif (cleaned_data.get(field) != response
+        elif (cleaned_data.get(field) not in responses
                 and cleaned_data.get(field_applicable) == NOT_APPLICABLE):
             raise forms.ValidationError({
                 field_applicable:
