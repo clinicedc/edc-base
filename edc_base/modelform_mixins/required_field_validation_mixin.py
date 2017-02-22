82a7879927b56fe96cd0ef3e0c3029bfdd23ff54
erikvw <ew2789@gmail.com>
2017-02-22 21:58:45 +0200

    add inverse option

--- edc_base/modelform_mixins/required_field_validation_mixin.py
+++ edc_base/modelform_mixins/required_field_validation_mixin.py
@@ -37,20 +37,22 @@ class RequiredFieldValidationMixin:
                     not_required_msg or 'This field is not required.'})
 
     def not_required_if(self, *responses, field=None, field_required=None,
-                        required_msg=None, not_required_msg=None, **kwargs):
+                        required_msg=None, not_required_msg=None,
+                        inverse=None, **kwargs):
         """Raises an exception or returns False.
 
         if field NOT in responses then field_required is required.
         """
         cleaned_data = self.cleaned_data
+        inverse = True if inverse is None else inverse
         if field in cleaned_data and field_required in cleaned_data:
             if (self.cleaned_data.get(field) in responses
                     and self.cleaned_data.get(field_required)):
                 raise forms.ValidationError({
                     field_required:
                     not_required_msg or 'This field is not required.'})
-            elif (self.cleaned_data.get(field) not in responses
-                    and not self.cleaned_data.get(field_required)):
+            elif inverse and (self.cleaned_data.get(field) not in responses
+                              and not self.cleaned_data.get(field_required)):
                 raise forms.ValidationError({
                     field_required:
                     required_msg or 'This field is required.'})
