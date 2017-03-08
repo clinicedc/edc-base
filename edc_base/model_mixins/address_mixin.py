7e3ae84d0596b0c5a0b828b6cd047b0b52c88668
erikvw <ew2789@gmail.com>
2017-03-08 17:22:20 +0200

    add preload data class

--- edc_base/model_mixins/address_mixin.py
+++ edc_base/model_mixins/address_mixin.py
@@ -3,25 +3,29 @@ from django.db import models
 
 class AddressMixin(models.Model):
 
+    contact_name = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True)
+
     address = models.CharField(
         verbose_name='Address',
-        null=True,
-        blank=True,
         max_length=50)
 
     postal_code = models.CharField(
-        null=True,
-        blank=True,
+        default='0000',
         max_length=50)
 
     city = models.CharField(
+        max_length=50)
+
+    state = models.CharField(
+        verbose_name='State or Province',
         null=True,
         blank=True,
         max_length=50)
 
     country = models.CharField(
-        null=True,
-        blank=True,
         max_length=50)
 
     telephone = models.CharField(
