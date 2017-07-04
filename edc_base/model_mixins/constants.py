f15ac1f5d560b7e152938f786f3effec55562aa3
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-07-04 12:39:32 +0200

    update fields to ignore for registrations

--- edc_base/model_mixins/constants.py
+++ edc_base/model_mixins/constants.py
@@ -5,4 +5,4 @@ BASE_UUID_MODEL_UPDATE_FIELDS = ['revision', ]
 DEFAULT_BASE_FIELDS = [
     'id', 'created', 'modified', 'user_created',
     'user_modified', 'hostname_created', 'hostname_modified',
-    'revision']
+    'revision', 'device_created', 'device_modified']
