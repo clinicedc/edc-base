870e8d308255f9f52bb2a57d6272d58722957921
erikvw <ew2789@gmail.com>
2017-07-27 11:20:48 +0200

    fix url mixin not adding correct default admin site name

--- edc_base/model_mixins/url_mixin.py
+++ edc_base/model_mixins/url_mixin.py
@@ -1,15 +1,26 @@
 from django.apps import apps as django_apps
 from django.urls import reverse
 from django.db import models
+from django.urls.exceptions import NoReverseMatch
+
+
+class UrlMixinNoReverseMatch(Exception):
+    pass
 
 
 class UrlMixin(models.Model):
 
     def get_absolute_url(self):
-        if self.id:
-            absolute_url = reverse(self.admin_url_name, args=(str(self.id),))
-        else:
-            absolute_url = reverse(self.admin_url_name)
+        try:
+            if self.id:
+                absolute_url = reverse(
+                    self.admin_url_name, args=(str(self.id),))
+            else:
+                absolute_url = reverse(self.admin_url_name)
+        except NoReverseMatch as e:
+            raise UrlMixinNoReverseMatch(
+                f'{e}. Perhaps define AppConfig.admin_site_name or '
+                'directly on model.ADMIN_SITE_NAME.')
         return absolute_url
 
     @property
@@ -20,10 +31,15 @@ class UrlMixin(models.Model):
             mode = 'change'
         else:
             mode = 'add'
-        return f'{self.admin_site_name}:{self._meta.app_label}_{self._meta.object_name.lower()}_{mode}'
+        return (f'{self.admin_site_name}:'
+                f'{self._meta.app_label}_{self._meta.object_name.lower()}_{mode}')
 
     @property
     def admin_site_name(self):
+        """Returns the admin url namespace for this model.
+
+        e.g. for module plot the default would be 'plot_admin'.
+        """
         try:
             # model specific
             admin_site_name = self.ADMIN_SITE_NAME
@@ -35,7 +51,7 @@ class UrlMixin(models.Model):
                     app_label).admin_site_name
             except AttributeError:
                 # default
-                admin_site_name = 'admin'
+                admin_site_name = f'{self._meta.app_label}_admin'
         return admin_site_name
 
     class Meta:
