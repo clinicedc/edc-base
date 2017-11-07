e0857d4b2b9f8c94b18e191c4bf98e9fab0d61d6
erikvw <ew2789@gmail.com>
2017-11-07 12:39:04 +0200

    default appointment model on fieldset mixin

--- edc_base/fieldsets/fieldsets_modeladmin_mixin.py
+++ edc_base/fieldsets/fieldsets_modeladmin_mixin.py
@@ -139,8 +139,7 @@ class FieldsetsModelAdminMixin(admin.ModelAdmin):
     def get_appointment(self, request):
         """Returns the appointment instance for this request or None.
         """
-        appointment_model_cls = django_apps.get_model(
-            django_apps.get_model(self.appointment_model))
+        appointment_model_cls = django_apps.get_model(self.appointment_model)
         try:
             return appointment_model_cls.objects.get(
                 pk=request.GET.get('appointment'))
