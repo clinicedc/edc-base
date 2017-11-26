54c9cc8df6b36dd96267787a58b719b1a45e500d
erikvw <ew2789@gmail.com>
2017-11-26 19:47:06 +0200

    improve exception message, move out model_admin tests

--- edc_base/apps.py
+++ edc_base/apps.py
@@ -14,7 +14,8 @@ style = color_style()
 
 
 def activate_foreign_keys(sender, connection, **kwargs):
-    """Enable integrity constraint with sqlite."""
+    """Enable integrity constraint with sqlite.
+    """
     if connection.vendor == 'sqlite':
         cursor = connection.cursor()
         cursor.execute('PRAGMA foreign_keys = ON;')
--- edc_base/views/administration_view.py
+++ edc_base/views/administration_view.py
@@ -2,16 +2,13 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
-from edc_dashboard.view_mixins import AppConfigViewMixin
 from edc_navbar import NavbarViewMixin
 
 from ..view_mixins import AdministrationViewMixin, EdcBaseViewMixin
 
 
-class AdministrationView(EdcBaseViewMixin, AppConfigViewMixin,
+class AdministrationView(EdcBaseViewMixin,
                          NavbarViewMixin, AdministrationViewMixin, TemplateView):
-
-    app_config_name = settings.APP_NAME
 
     navbar_name = settings.APP_NAME
     navbar_selected_item = 'administration'
