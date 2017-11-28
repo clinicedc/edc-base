b74f8fd192cec6f8e5040fa950c67577d47f31b2
erikvw <ew2789@gmail.com>
2017-11-28 21:58:17 +0200

    dont discover list data modules on migrate

--- edc_base/site_list_data.py
+++ edc_base/site_list_data.py
@@ -19,30 +19,31 @@ class SiteListData():
     """
 
     def autodiscover(self, module_name=None, verbose=True):
-        module_name = module_name or 'list_data'
-        writer = sys.stdout.write if verbose else lambda x: x
-        style = color_style()
-        writer(f' * checking for site {module_name} ...\n')
-        for app in django_apps.app_configs:
-            writer(f' * searching {app}           \r')
-            try:
-                mod = import_module(app)
+        if 'migrate' not in sys.argv and 'makemigrations' not in sys.argv:
+            module_name = module_name or 'list_data'
+            writer = sys.stdout.write if verbose else lambda x: x
+            style = color_style()
+            writer(f' * checking for site {module_name} ...\n')
+            for app in django_apps.app_configs:
+                writer(f' * searching {app}           \r')
                 try:
-                    import_module(f'{app}.{module_name}')
-                    writer(
-                        f' * loading \'{module_name}\' from \'{app}\'\n')
-                except PreloadDataError as e:
-                    writer(f'   - loading {app}.{module_name} ... ')
-                    writer(style.ERROR(f'ERROR! {e}\n'))
-                except ImportError as e:
-                    if module_has_submodule(mod, module_name):
-                        raise SiteListDataError(e)
-            except ImportError:
-                pass
-            except Exception as e:
-                raise SiteListDataError(
-                    f'{e.__class__.__name__} was raised when loading {module_name}. '
-                    f'Got {e} See {app}.{module_name}')
+                    mod = import_module(app)
+                    try:
+                        import_module(f'{app}.{module_name}')
+                        writer(
+                            f' * loading \'{module_name}\' from \'{app}\'\n')
+                    except PreloadDataError as e:
+                        writer(f'   - loading {app}.{module_name} ... ')
+                        writer(style.ERROR(f'ERROR! {e}\n'))
+                    except ImportError as e:
+                        if module_has_submodule(mod, module_name):
+                            raise SiteListDataError(e)
+                except ImportError:
+                    pass
+                except Exception as e:
+                    raise SiteListDataError(
+                        f'{e.__class__.__name__} was raised when loading {module_name}. '
+                        f'Got {e} See {app}.{module_name}')
 
 
 site_list_data = SiteListData()
