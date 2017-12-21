48d06a2d16812e0550239464112268f635b7e61d
erikvw <ew2789@gmail.com>
2017-12-21 14:30:24 +0200

    minor

--- edc_base/__init__.py
+++ edc_base/__init__.py
@@ -1,1 +1,3 @@
-from .constants import BASE_MODEL_UPDATE_FIELDS, BASE_UUID_MODEL_UPDATE_FIELDS, DEFAULT_BASE_FIELDS
+from .constants import BASE_MODEL_UPDATE_FIELDS, BASE_UUID_MODEL_UPDATE_FIELDS
+from .constants import DEFAULT_BASE_FIELDS
+from .utils import get_utcnow
