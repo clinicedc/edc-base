0bedc67b61b70d91c227aee6701efc6d1f900f5f
erikvw <ew2789@gmail.com>
2018-01-03 11:48:05 +0200

    change get to filter

--- edc_base/sites/add_or_update_django_sites.py
+++ edc_base/sites/add_or_update_django_sites.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 def add_or_update_django_sites(apps, edc_sites=None, edc_fqdn=None):
     Site = apps.get_model('sites', 'Site')
-    Site.objects.get(name='example.com').delete()
+    Site.objects.filter(name='example.com').delete()
     for site_id, site_name in edc_sites:
         try:
             site_obj = Site.objects.get(pk=site_id)
