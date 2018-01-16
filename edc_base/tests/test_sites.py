a4885b6c09cfd6d4aab6549b72f601378dcd8cc5
erikvw <ew2789@gmail.com>
2018-01-16 12:55:05 +0200

    PEP8

--- edc_base/tests/test_sites.py
+++ edc_base/tests/test_sites.py
@@ -1,28 +1,25 @@
+from django.contrib.sites.models import Site
 from django.test import TestCase, tag
+from django.test.utils import override_settings
 
 from .models import TestModelWithSite
 from .site_test_case_mixin import SiteTestCaseMixin
-from django.test.utils import override_settings
-from django.contrib.sites.models import Site
 
 
 class TestSites(SiteTestCaseMixin, TestCase):
 
-    @tag('1')
     @override_settings(SITE_ID=20)
     def test_20(self):
         obj = TestModelWithSite.objects.create()
         self.assertEqual(obj.site.pk, 20)
         self.assertEqual(obj.site.pk, Site.objects.get_current().pk)
 
-    @tag('1')
     @override_settings(SITE_ID=30)
     def test_30(self):
         obj = TestModelWithSite.objects.create()
         self.assertEqual(obj.site.pk, 30)
         self.assertEqual(obj.site.pk, Site.objects.get_current().pk)
 
-    @tag('1')
     @override_settings(SITE_ID=30)
     def test_override_current(self):
         site = Site.objects.get(pk=40)
