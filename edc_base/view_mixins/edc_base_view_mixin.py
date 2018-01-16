94100d05ecb54ba9a8f914a1477443d03c3d1e02
erikvw <ew2789@gmail.com>
2018-01-16 12:49:30 +0200

    update auth urls, views and templates to use django classes

--- edc_base/view_mixins/edc_base_view_mixin.py
+++ edc_base/view_mixins/edc_base_view_mixin.py
@@ -1,22 +1,18 @@
 from django.apps import apps as django_apps
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.sites.shortcuts import get_current_site
 from django.views.generic.base import ContextMixin
 from django_revision.views import RevisionMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class EdcBaseViewMixin(RevisionMixin, ContextMixin):
+class EdcBaseViewMixin(LoginRequiredMixin, RevisionMixin, ContextMixin):
     """Mixes in common template variables for the footer, etc.
     """
+    # login_url = '/login/'
 
     def get_context_data(self, **kwargs):
-
-        self.request.session.set_test_cookie()
-        if not self.request.session.test_cookie_worked():
-            messages.add_message(
-                self.request, messages.ERROR, 'Please enable cookies.')
-        self.request.session.delete_test_cookie()
+        super().get_context_data(**kwargs)
         app_config = django_apps.get_app_config('edc_base')
         edc_device_app_config = django_apps.get_app_config('edc_device')
         context = super().get_context_data(**kwargs)
@@ -29,7 +25,6 @@ class EdcBaseViewMixin(RevisionMixin, ContextMixin):
             'institution': app_config.institution,
             'license': app_config.license,
             'project_name': app_config.project_name,
-            'site': get_current_site(self.request),
         })
         if settings.DEBUG:
             messages.add_message(
--- edc_base/views/__init__.py
+++ edc_base/views/__init__.py
@@ -1,4 +1,2 @@
 from .administration_view import AdministrationView
 from .home_view import HomeView
-from .login_view import LoginView
-from .logout_view import LogoutView
