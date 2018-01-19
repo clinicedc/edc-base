d3327d6ff8ab7c4793a9aad5e4c81620317af65e
erikvw <ew2789@gmail.com>
2018-01-19 13:24:23 +0200

    add SiteError exception

--- edc_base/sites/__init__.py
+++ edc_base/sites/__init__.py
@@ -3,3 +3,7 @@ from .forms import SiteModelFormMixin
 from .managers import CurrentSiteManager
 from .site_model_mixin import SiteModelMixin
 from .view_mixins import SiteQuerysetViewMixin
+
+
+class SiteError(Exception):
+    pass
