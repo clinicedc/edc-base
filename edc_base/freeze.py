4d0800bd419607dab0d009dc1d757bd1c1b41b79
erikvw <ew2789@gmail.com>
2018-07-24 15:23:14 +0200

    fix freeze

--- edc_base/freeze.py
+++ edc_base/freeze.py
@@ -1,11 +1,10 @@
-from subprocess import Popen, PIPE
+import subprocess
 
-with Popen(["pip", "freeze"], stdout=PIPE, encoding='utf8') as proc:
-    freeze = proc.stdout.read()
-
-
+proc = subprocess.Popen(['pip', 'freeze'], stdout=subprocess.PIPE)
+freeze = proc.communicate()[0]
 freeze = freeze.split('\n')
-edc_packages = [x for x in freeze if 'clinicedc' in x or 'erikvw' in x]
+edc_packages = [
+    x for x in freeze if 'clinicedc' in x or 'erikvw' in x or 'edc-'in x]
 third_party_packages = [x for x in freeze if x and x not in edc_packages]
 edc_packages.sort()
 third_party_packages.sort()
