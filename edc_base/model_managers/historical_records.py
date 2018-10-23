646e4f810f912b7e84b9d2de7b1352fe5b8f712a
erikvw <ew2789@gmail.com>
2018-10-22 21:28:12 -0500

    no longer overriding post_save, post_delete, create_historical_record

--- edc_base/model_managers/historical_records.py
+++ edc_base/model_managers/historical_records.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.db.models.fields import AutoField
 from simple_history.models import HistoricalRecords as SimpleHistoricalRecords
-
-from ..utils import get_utcnow
+from django.utils.timezone import now
 
 
 class SerializableModelManager(models.Manager):
@@ -56,31 +55,3 @@ class HistoricalRecords(SimpleHistoricalRecords):
         extra_fields.update({'history_id': self.get_history_id_field(model)})
         extra_fields.update({'natural_key': lambda x: (x.history_id, )})
         return extra_fields
-
-    def post_save(self, instance, created, **kwargs):
-        """Overridden to include \'using\'.
-        """
-        if not created and hasattr(instance, 'skip_history_when_saving'):
-            return
-        if not kwargs.get('raw', False):
-            self.create_historical_record(
-                instance, created and '+' or '~', using=kwargs.get('using'))
-
-    def post_delete(self, instance, **kwargs):
-        """Overridden to include \'using\'.
-        """
-        self.create_historical_record(instance, '-', using=kwargs.get('using'))
-
-    def create_historical_record(self, instance, history_type, **kwargs):
-        """Overridden to include \'using\'.
-        """
-        history_date = getattr(instance, '_history_date', get_utcnow())
-        history_user = self.get_history_user(instance)
-        manager = getattr(instance, self.manager_name)
-        attrs = {}
-        for field in instance._meta.fields:
-            attrs[field.attname] = getattr(instance, field.attname)
-        manager.using(kwargs.get('using')).create(
-            history_date=history_date,
-            history_type=history_type,
-            history_user=history_user, **attrs)
