0175bb00948d68867a9ce2bb3118ff99c56b7ab7
erikvw <ew2789@gmail.com>
2018-11-07 19:52:37 -0600

    replace my UUID field functionality with that from simple_history

--- edc_base/model_managers/historical_records.py
+++ edc_base/model_managers/historical_records.py
@@ -1,7 +1,7 @@
+import uuid
+
 from django.db import models
-from django.db.models.fields import AutoField
 from simple_history.models import HistoricalRecords as SimpleHistoricalRecords
-from django.utils.timezone import now
 
 
 class SerializableModelManager(models.Manager):
@@ -24,34 +24,21 @@ class SerializableModel(models.Model):
 class HistoricalRecords(SimpleHistoricalRecords):
 
     """HistoricalRecords that uses a UUID primary key
-    and has a natural key method.
+    and has a natural key method available for serialization.
     """
 
     model_cls = SerializableModel
 
     def __init__(self, **kwargs):
+        """Defaults use of UUIDField instead of AutoField.
+        """
         kwargs.update(bases=(self.model_cls, ))
+        kwargs.update(history_id_field=models.UUIDField(default=uuid.uuid4))
         super().__init__(**kwargs)
 
-    def get_history_id_field(self, model):
-        """Return a field instance without initially assuming
-        it should be AutoField.
-
-        For example, primary key is UUIDField(
-            primary_key=True, default=uuid.uuid4).
-        """
-        try:
-            field = [
-                field for field in model._meta.fields if field.primary_key][0]
-            field = field.__class__(primary_key=True, default=field.default)
-        except (IndexError, TypeError):
-            field = AutoField(primary_key=True)
-        return field
-
     def get_extra_fields(self, model, fields):
-        """Overridden to set history_id (to UUIDField).
+        """Overridden to add natural key.
         """
         extra_fields = super().get_extra_fields(model, fields)
-        extra_fields.update({'history_id': self.get_history_id_field(model)})
         extra_fields.update({'natural_key': lambda x: (x.history_id, )})
         return extra_fields
