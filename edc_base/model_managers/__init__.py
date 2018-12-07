1e6d1d888794a54bf113c91e005f57897143c425
erikvw <ew2789@gmail.com>
2018-12-06 20:09:10 -0600

    move listmodelmixn to edc-list-data

--- edc_base/model_managers/__init__.py
+++ edc_base/model_managers/__init__.py
@@ -1,3 +1,2 @@
 from .historical_records import HistoricalRecords
 from .history_manager_mixin import HistoryManagerMixin
-from .list_model_manager import ListModelManager
--- edc_base/model_mixins/__init__.py
+++ edc_base/model_mixins/__init__.py
@@ -5,6 +5,5 @@ from .address_mixin import AddressMixin
 from .base_model import BaseModel
 from .base_uuid_model import BaseUuidModel
 from .form_as_json_model_mixin import FormAsJSONModelMixin
-from .list_model_mixin import ListModelMixin
 from .report_status_model_mixin import ReportStatusModelMixin
 from .url_mixin import UrlMixin
